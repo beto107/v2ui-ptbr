b6e41dae309d0d9eb84d80e68e08cf04f4f0eb7c
Andley S <39422775+Andley302@users.noreply.github.com>
2021-08-08 14:48:04 -0300

    Update install_v2ray.sh

--- install_v2ray.sh
+++ install_v2ray.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 apt update;
-curl -L https://github.com/andley302/v2ui-ptbr/master/v2-ui-linux-amd64.tar.gz  -o v2-ui-linux.tar.gz;
+curl -L https://github.com/sprov065/v2-ui/releases/download/5.5.2/v2-ui-linux-amd64.tar.gz  -o v2-ui-linux.tar.gz;
 cd /root/;
 mv v2-ui-linux.tar.gz /usr/local/;
 cd /usr/local/;
@@ -12,5 +12,5 @@ cp -f v2-ui.service /etc/systemd/system/;
 systemctl daemon-reload;
 systemctl enable v2-ui;
 systemctl restart v2-ui;
-curl -o /usr/bin/v2-ui -Ls https://github.com/andley302/v2ui-ptbr/master/v2-ui.sh;
+curl -o /usr/bin/v2-ui -Ls https://raw.githubusercontent.com/Andley302/v2ui-ptbr/main/v2-ui.sh;
 chmod +x /usr/bin/v2-ui;
